b4bdae2c75a4a43a61cebe19ebd610c5b7a173b4
Jonathan <mbtjonathan@gmail.com>
2021-05-03 19:59:18 +0300

    Add an argument for graph_properties

--- src/graph.py
+++ src/graph.py
@@ -18,7 +18,7 @@ all_hashtags = []
 all_edges = []
 
 #reading the csv file containing all the tweets containing the #parking, #parkinglot, ...
-tweets = pd.read_csv("../resources/20K_parking_data.csv")
+tweets = pd.read_csv(data_filename)
 
 
 for tweet in tweets.iloc:
@@ -59,75 +59,77 @@ if graph_image:
     print("\n *** Graph exported as an image. *** \n")
 
 # Computation Graph
-CG = nx.Graph()
-CG.add_nodes_from(hashtag_nodes)
-CG.add_edges_from(hashtag_edges)
-
-print("\n *** Global properties of Graph *** \n")
-graph_data = {}
-graph_data['node_number'] = len(CG.nodes)
-print("Number of nodes: ", graph_data['node_number'])
-graph_data['edge_number'] = len(CG.edges)
-print("Number of edges: ", graph_data['edge_number'])
-degree_centrality = nx.degree_centrality(CG)
-graph_data['avg_degree_centrality'] = st.mean(degree_centrality[k] for k in degree_centrality)
-print("Average degree centrality: ", graph_data['avg_degree_centrality'])
-graph_data['variance_degree_centrality'] = st.variance(degree_centrality[k] for k in degree_centrality)
-print("Variance degree centrality: ", graph_data['variance_degree_centrality'])
-betweenness_centrality = nx.betweenness_centrality(CG)
-graph_data['avg_betweenness_centrality'] = st.mean(betweenness_centrality[k] for k in betweenness_centrality)
-print("Average betweenness centrality: ", graph_data['avg_betweenness_centrality'])
-graph_data['variance_betweenness_centrality'] = st.variance(betweenness_centrality[k] for k in betweenness_centrality)
-print("Variance betweenness centrality: ", graph_data['variance_betweenness_centrality'])
-components = [CG.subgraph(c).copy() for c in nx.connected_components(CG)]
-graph_data['diameter'] = max([nx.diameter(x) for x in components])
-print("Diameter: ", graph_data['diameter'])
-graph_data['clustering_coefficient'] = nx.average_clustering(CG)
-print("Clustering Coefficient:", graph_data['clustering_coefficient'])
-graph_data['size_lg_component'] = len(max(components, key=len))
-print("Size of largest component: ", graph_data['size_lg_component'])
-
-graph_properties_db = pd.DataFrame(graph_data, index=[0])
-graph_properties_db.to_csv("graph_properties.csv")
-
-""" from itertools import chain
-for C in (CG.subgraph(c).copy() for c in nx.connected_components(CG)):
-    path_lengths = (x.values() for x in dict(nx.shortest_path_length(C)).values())
-    print(path_lengths)
-    tmp = sum(chain.from_iterable(path_lengths))
-    tmp /= graph_data['node_number']*(graph_data['node_number']-1)
-    #print(nx.average_shortest_path_length(C))
-    print("average_shortest_path_length: ", tmp) """
-
-# 5 highest ranked nodes according to several criterias
-print("\n *** Top 5 according to different criterias *** \n")
-hgr_nodes = {}
-hgr_nodes['degree_centrality'] = maxN(degree_centrality, 5)
-print("5 highest nodes based on degree centrality: ", hgr_nodes['degree_centrality'])
-pr_centrality = nx.pagerank(CG, alpha=0.95)
-hgr_nodes['page_rank_centrality'] = maxN(pr_centrality, 5)
-print("5 highest nodes based on page rank centrality: ", hgr_nodes['page_rank_centrality'])
-closeness_centrality = nx.closeness_centrality(CG)
-hgr_nodes['closeness_centrality'] = maxN(closeness_centrality, 5)
-print("5 highest nodes based on closeness centrality: ", hgr_nodes['closeness_centrality'])
-hgr_nodes['betweenness_centrality'] = maxN(betweenness_centrality, 5)
-print("5 highest nodes based on betweenness centrality: ", hgr_nodes['betweenness_centrality'])
-
-# Plot histogram of distributions 
-print("\n *** Distributions *** \n")
-fig, ax = plt.subplots(2, 2, figsize=(10,8))
-ax[0][0].hist(list(degree_centrality.values()), bins=int(node_number/2))
-ax[0][0].set_title("Distribution of the degree centrality")
-ax[0][1].hist(list(nx.clustering(CG).values()), bins=int(node_number/2))
-ax[0][1].set_title("Distribution of the local clustering coefficient")
-ax[1][0].hist(list(betweenness_centrality.values()), bins=int(node_number/2))
-ax[1][0].set_title("Distribution of the betweenness centrality")
-ax[1][1].hist(list(closeness_centrality.values()), bins=int(node_number/2))
-ax[1][1].set_title("Distribution of the closeness centrality")
-plt.show()
+if graph_properties:
+    CG = nx.Graph()
+    CG.add_nodes_from(hashtag_nodes)
+    CG.add_edges_from(hashtag_edges)
+    
+    print("\n *** Global properties of Graph *** \n")
+    graph_data = {}
+    graph_data['node_number'] = len(CG.nodes)
+    print("Number of nodes: ", graph_data['node_number'])
+    graph_data['edge_number'] = len(CG.edges)
+    print("Number of edges: ", graph_data['edge_number'])
+    degree_centrality = nx.degree_centrality(CG)
+    graph_data['avg_degree_centrality'] = st.mean(degree_centrality[k] for k in degree_centrality)
+    print("Average degree centrality: ", graph_data['avg_degree_centrality'])
+    graph_data['variance_degree_centrality'] = st.variance(degree_centrality[k] for k in degree_centrality)
+    print("Variance degree centrality: ", graph_data['variance_degree_centrality'])
+    betweenness_centrality = nx.betweenness_centrality(CG) 
+    graph_data['avg_betweenness_centrality'] = st.mean(betweenness_centrality[k] for k in betweenness_centrality)
+    print("Average betweenness centrality: ", graph_data['avg_betweenness_centrality'])
+    graph_data['variance_betweenness_centrality'] = st.variance(betweenness_centrality[k] for k in betweenness_centrality)
+    print("Variance betweenness centrality: ", graph_data['variance_betweenness_centrality'])
+    components = [CG.subgraph(c).copy() for c in nx.connected_components(CG)]
+    graph_data['diameter'] = max([nx.diameter(x) for x in components])
+    print("Diameter: ", graph_data['diameter'])
+    graph_data['clustering_coefficient'] = nx.average_clustering(CG)
+    print("Clustering Coefficient:", graph_data['clustering_coefficient'])
+    graph_data['size_lg_component'] = len(max(components, key=len))
+    print("Size of largest component: ", graph_data['size_lg_component'])
+    
+    graph_properties_db = pd.DataFrame(graph_data, index=[0])
+    graph_properties_db.to_csv("graph_properties.csv")
+    
+    """ from itertools import chain
+    for C in (CG.subgraph(c).copy() for c in nx.connected_components(CG)):
+        path_lengths = (x.values() for x in dict(nx.shortest_path_length(C)).values())
+        print(path_lengths)
+        tmp = sum(chain.from_iterable(path_lengths))
+        tmp /= graph_data['node_number']*(graph_data['node_number']-1)
+        #print(nx.average_shortest_path_length(C))
+        print("average_shortest_path_length: ", tmp) """
+    
+    # 5 highest ranked nodes according to several criterias
+    print("\n *** Top 5 according to different criterias *** \n")
+    hgr_nodes = {}
+    hgr_nodes['degree_centrality'] = maxN(degree_centrality, 5)
+    print("5 highest nodes based on degree centrality: ", hgr_nodes['degree_centrality'])
+    pr_centrality = nx.pagerank(CG, alpha=0.95)
+    hgr_nodes['page_rank_centrality'] = maxN(pr_centrality, 5)
+    print("5 highest nodes based on page rank centrality: ", hgr_nodes['page_rank_centrality'])
+    closeness_centrality = nx.closeness_centrality(CG)
+    hgr_nodes['closeness_centrality'] = maxN(closeness_centrality, 5)
+    print("5 highest nodes based on closeness centrality: ", hgr_nodes['closeness_centrality'])
+    hgr_nodes['betweenness_centrality'] = maxN(betweenness_centrality, 5)
+    print("5 highest nodes based on betweenness centrality: ", hgr_nodes['betweenness_centrality'])
+    
+    # Plot histogram of distributions 
+    print("\n *** Distributions *** \n")
+    fig, ax = plt.subplots(2, 2, figsize=(12,8))
+    ax[0][0].hist(list(degree_centrality.values()), bins=int(node_number/2))
+    ax[0][0].set_title("Distribution of the degree centrality")
+    ax[0][1].hist(list(nx.clustering(CG).values()), bins=int(node_number/2))
+    ax[0][1].set_title("Distribution of the local clustering coefficient")
+    ax[1][0].hist(list(betweenness_centrality.values()), bins=int(node_number/2))
+    ax[1][0].set_title("Distribution of the betweenness centrality")
+    ax[1][1].hist(list(closeness_centrality.values()), bins=int(node_number/2))
+    ax[1][1].set_title("Distribution of the closeness centrality")
+    plt.show()
 
 
 if not not_displayed:
+    plt.figure(figsize=(12,8))
     plt.plot()
     pos = nx.drawing.nx_pydot.graphviz_layout(G, 'fdp')
     nx.draw(G, pos=pos, with_labels=True)
